9d953527f3771d611e766233c428a237a205ac8b
Aaron Lun <alun@wehi.edu.au>
2017-06-15 19:06:06 +0100

    Updated Zeisel timings with rectangular chunks.

--- timings/real/pics/make_plots.R
+++ timings/real/pics/make_plots.R
@@ -1,13 +1,17 @@
 # This makes bar plots of row/column access times.
 
 pdf("zeisel_col.pdf")
-col.times <- read.table("../timings_col.txt", header=TRUE)
+par(mar=c(7.1, 5.1, 2.1, 2.1))
+col.times <- read.table("../timings_col.txt", header=TRUE, sep="\t", stringsAsFactors=FALSE)
 barplot(setNames(col.times$Time, col.times$Type), ylab="Time (ms)", cex.axis=1, cex.lab=1.4, cex.names=1.4, 
-        cex.main=1.4, main="Column access", log="y", ylim=range(col.times$Time)*c(0.5, 1.5))
+        names.arg=sub(" ", "\n", col.times$Type), las=2,
+        cex.main=1.4, main="Column access")
 dev.off()
 
 pdf("zeisel_row.pdf")
-row.times <- read.table("../timings_row.txt", header=TRUE)
+par(mar=c(7.1, 5.1, 2.1, 2.1))
+row.times <- read.table("../timings_row.txt", header=TRUE, sep="\t")
 barplot(setNames(row.times$Time, row.times$Type), ylab="Time (ms)", cex.axis=1, cex.lab=1.4, cex.names=1.4, 
-        cex.main=1.4, main="Row access", log="y", ylim=range(row.times$Time)*c(0.5, 1.5))
+        names.arg=sub(" ", "\n", col.times$Type), las=2,
+        cex.main=1.4, main="Row access")
 dev.off()
--- timings/real/zeisel_time.R
+++ timings/real/zeisel_time.R
@@ -12,67 +12,50 @@ library(Matrix)
 sparse.counts <- as(Matrix(dense.counts), "dgCMatrix")
 
 library(HDF5Array)
-fpath <- c("bycol.h5", "byrow.h5")
+fpath <- c("bycol.h5", "byrow.h5", "rect.h5")
 for (f in fpath) { if (file.exists(f)) { unlink(f) } }
-hdf5.by.col <- writeHDF5Array(dense.counts, fpath[1], name="yay", chunk_dim=c(5000, 1), level=6)
+hdf5.by.col <- writeHDF5Array(dense.counts, fpath[1], name="yay", chunk_dim=c(nrow(dense.counts), 1), level=6)
 hdf5.by.row <- writeHDF5Array(dense.counts, fpath[2], name="yay", chunk_dim=c(1, ncol(dense.counts)), level=6)
+hdf5.rect <- writeHDF5Array(dense.counts, fpath[3], name="yay", chunk_dim=c(200, 200), level=6)
 
 write.table(data.frame(Sizes=c(simple=object.size(dense.counts)/1e3,
                                sparse=object.size(sparse.counts)/1e3,
                                HDF5.col=object.size(hdf5.by.col)/1e3,
                                HDF5.row=object.size(hdf5.by.row)/1e3,
+                               HDF5.rect=object.size(hdf5.rect)/1e3,
                                HDF5.col.file=file.info(fpath[1])$size/1e3,
-                               HDF5.row.file=file.info(fpath[2])$size/1e3)),
+                               HDF5.row.file=file.info(fpath[2])$size/1e3,
+                               HDF5.rect.file=file.info(fpath[3])$size/1e3)),                      ,
             col.names=NA, quote=FALSE, sep="\t", file="memory.txt")
-
-
-# Setting up rechunking values.
-
-repath <- "rechunked.h5"
-library(usingRhdf5lib)
-rechunk <- function(incoming, outname, byrow=TRUE) {
-    cxxfun <- ifelse(byrow, "pack_by_row", "pack_by_col")
-    tolerance <- 1e8/8
-    .Call(cxxfun, incoming@seed@file, incoming@seed@name, outname, tolerance, PACKAGE="usingRhdf5lib")
-    HDF5Array(outname, incoming@seed@name)
-}
 
 # Running the column access timings.
 
 library(beachtime)
-dense.time <- sparse.time <- hdf5.time <- rechunk.time <- numeric(10)
+dense.time <- sparse.time <- hdf5.time <- rect.time <- numeric(10)
 for (it in 1:10) { 
     dense.time[it] <- timeExprs(BeachmatColSum(dense.counts), times=10)
     sparse.time[it] <- timeExprs(BeachmatColSum(sparse.counts), times=10)
     hdf5.time[it] <- timeExprs(BeachmatColSum(hdf5.by.col), times=1)
-    rechunk.time[it] <- timeExprs({ 
-        rechunked <- rechunk(hdf5.by.row, repath, byrow=FALSE)
-        BeachmatColSum(rechunked) 
-    }, times=1)
-    unlink(repath)
+    rect.time[it] <- timeExprs(BeachmatColSum(hdf5.rect), times=1)
 }
 
 writeToFile(Type="simple", timings=dense.time, file="timings_col.txt", overwrite=TRUE)
 writeToFile(Type="sparse", timings=sparse.time, file="timings_col.txt", overwrite=FALSE)
-writeToFile(Type="HDF5", timings=hdf5.time, file="timings_col.txt", overwrite=FALSE)
-writeToFile(Type="rechunk", timings=rechunk.time, file="timings_col.txt", overwrite=FALSE)
+writeToFile(Type="HDF5 (column)", timings=hdf5.time, file="timings_col.txt", overwrite=FALSE)
+writeToFile(Type="HDF5 (rectangle)", timings=rect.time, file="timings_col.txt", overwrite=FALSE)
 
 # Running the row access timings.
 
-dense.time <- sparse.time <- hdf5.time <- rechunk.time <- numeric(10)
+dense.time <- sparse.time <- hdf5.time <- rect.time <- numeric(10)
 for (it in 1:10) { 
     dense.time[it] <- timeExprs(BeachmatRowSum(dense.counts), times=10)
     sparse.time[it] <- timeExprs(BeachmatRowSum(sparse.counts), times=10)
     hdf5.time[it] <- timeExprs(BeachmatRowSum(hdf5.by.row), times=1)
-    rechunk.time[it] <- timeExprs({ 
-        rechunked <- rechunk(hdf5.by.col, repath, byrow=TRUE)
-        BeachmatRowSum(rechunked) 
-    }, times=1)
-    unlink(repath)
+    rect.time[it] <- timeExprs(BeachmatRowSum(hdf5.rect), times=1) 
 }
 
 writeToFile(Type="simple", timings=dense.time, file="timings_row.txt", overwrite=TRUE)
 writeToFile(Type="sparse", timings=sparse.time, file="timings_row.txt", overwrite=FALSE)
-writeToFile(Type="HDF5", timings=hdf5.time, file="timings_row.txt", overwrite=FALSE)
-writeToFile(Type="rechunk", timings=rechunk.time, file="timings_row.txt", overwrite=FALSE)
+writeToFile(Type="HDF5 (row)", timings=hdf5.time, file="timings_row.txt", overwrite=FALSE)
+writeToFile(Type="HDF5 (rectangle)", timings=rect.time, file="timings_row.txt", overwrite=FALSE)
 
