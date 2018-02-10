544e631150c8d9f4b722af5b6dda28179da344a4
Aaron Lun <alun@wehi.edu.au>
2018-02-10 16:27:18 +0000

    Added non-consecutive row access tests for sparse matrices.

--- simulations/timings/sparse_matrix.R
+++ simulations/timings/sparse_matrix.R
@@ -38,9 +38,9 @@ for (ncells in c(1000, 2000, 5000, 10000)) {
 
 ncells <- 1000
 overwrite <- TRUE
-skip.arma <- FALSE
 for (ngenes in c(10000, 20000, 50000, 100000)) {
     for (density in c(0.01, 0.05, 0.1, 0.2)) { 
+        skip.arma <- !(density==0.01 || ngenes<=50000)
 
         naive.time <- row.time <- arma.time <- eigen.time <- def.time <- numeric(10)
         for (it in seq_len(10)) { 
@@ -48,9 +48,10 @@ for (ngenes in c(10000, 20000, 50000, 100000)) {
             dense.counts <- as.matrix(sparse.counts)
             row.time[it] <- timeExprs(BeachmatRowSum(sparse.counts))
             def.time[it] <- timeExprs(BeachmatRowSum(dense.counts))
-            naive.time[it] <- timeExprs(NaiveSparseRowSum(sparse.counts), times=1) # slow, so only once.
+            naive.time[it] <- timeExprs(NaiveSparseRowSum(sparse.counts), times=1)
 
-            # This takes some time, so will only run for the lowest number of genes, and once.
+            # This takes a lot of time, so will only run once. 
+            # We also skip the more complex timings. 
             if (!skip.arma) {
                 eigen.time[it] <- timeExprs(EigenRowSum(sparse.counts), times=1)
                 arma.time[it] <- timeExprs(ArmaRowSum(sparse.counts), times=1)
@@ -72,8 +73,61 @@ for (ngenes in c(10000, 20000, 50000, 100000)) {
                         timings=eigen.time, file="timings_sparse_row.txt", overwrite=overwrite)
         }
     }
-            
-    skip.arma <- TRUE
+}
+
+###########################
+# Ordered but non-consecutive row access.
+
+ncells <- 1000
+density <- 0.01
+overwrite <- TRUE
+for (ngenes in c(10000, 20000, 50000, 100000)) {
+    o <- seq_len(ngenes)
+    o <- o[order(o %% 5, o)] # ordered so that there are no consecutive runs.
+
+    naive.time <- row.time <- def.time <- numeric(10)
+    for (it in seq_len(10)) { 
+        sparse.counts <- rsparsematrix(ngenes, ncells, density)
+        dense.counts <- as.matrix(sparse.counts)
+        row.time[it] <- timeExprs(BeachmatRowSumRandom(sparse.counts, o), times=1)
+        def.time[it] <- timeExprs(BeachmatRowSumRandom(dense.counts, o))
+        naive.time[it] <- timeExprs(NaiveSparseRowSumRandom(sparse.counts, o), times=1)
+    }
+
+    writeToFile(Type="sparse (cached)", Ngenes=ngenes, Ncells=ncells, Density=density, 
+                timings=row.time, file="timings_sparse_row_ordered.txt", overwrite=overwrite)
+    overwrite <- FALSE 
+    writeToFile(Type="simple", Ngenes=ngenes, Ncells=ncells, Density=density, 
+                timings=def.time, file="timings_sparse_row_ordered.txt", overwrite=overwrite)
+    writeToFile(Type="sparse (naive)", Ngenes=ngenes, Ncells=ncells, Density=density, 
+                timings=naive.time, file="timings_sparse_row_ordered.txt", overwrite=overwrite)
+}
+
+###########################
+# Random row access.
+
+ncells <- 1000
+density <- 0.01
+overwrite <- TRUE
+for (ngenes in c(10000, 20000, 50000, 100000)) {
+    naive.time <- row.time <- def.time <- numeric(10)
+    for (it in seq_len(10)) { 
+        sparse.counts <- rsparsematrix(ngenes, ncells, density)
+        dense.counts <- as.matrix(sparse.counts)
+        o <- sample(ngenes)
+
+        row.time[it] <- timeExprs(BeachmatRowSumRandom(sparse.counts, o), times=1)
+        def.time[it] <- timeExprs(BeachmatRowSumRandom(dense.counts, o))
+        naive.time[it] <- timeExprs(NaiveSparseRowSumRandom(sparse.counts, o), times=1)
+    }
+
+    writeToFile(Type="sparse (cached)", Ngenes=ngenes, Ncells=ncells, Density=density, 
+                timings=row.time, file="timings_sparse_row_random.txt", overwrite=overwrite)
+    overwrite <- FALSE 
+    writeToFile(Type="simple", Ngenes=ngenes, Ncells=ncells, Density=density, 
+                timings=def.time, file="timings_sparse_row_random.txt", overwrite=overwrite)
+    writeToFile(Type="sparse (naive)", Ngenes=ngenes, Ncells=ncells, Density=density, 
+                timings=naive.time, file="timings_sparse_row_random.txt", overwrite=overwrite)
 }
 
 ###########################
