b05dd5b6b2e581a70737694e31c1213dd4b7bbc9
Aaron Lun <alun@wehi.edu.au>
2018-02-12 16:43:31 +0000

    Fleshed out comparison of C++ to block processing.

--- simulations/miscellaneous/versus_R.R
+++ simulations/miscellaneous/versus_R.R
@@ -1,28 +1,51 @@
-a <- matrix(rnorm(1000000), nrow=10000)
+library(beachtime)
+library(Matrix)
+library(HDF5Array)
 
-# (1) Totally unvectorized version of taking the rowSums.
-# Shown here to mimic the performance of a completely 
-# naive implementation of an arbitrary operation.
-system.time({
-    out <- numeric(nrow(a))
-    for (i in seq_len(nrow(a))) {
-        total <- 0
-        for (j in seq_len(ncol(a))) { 
-            total <- total + a[i,j]
+block.sizes <- c(10, 20, 50, 100, 200, 500, 1000)
+
+beach.time <- rep(list(numeric(10)), 3)
+default.time <- rep(list(matrix(0, 10, length(block.sizes))), 3)
+names(beach.time) <- names(default.time) <- c("dense", "sparse", "hdf5")
+
+for (it in 1:10) { 
+    for (mode in c("dense", "sparse", "hdf5")) { 
+        if (mode=="dense") {
+            X <- matrix(rnorm(1000000), nrow=100)
+        } else if (mode=="sparse") {
+            X <- rsparsematrix(nrow=100, ncol=10000, density=0.01)
+        } else {
+            fpath <- "a.h5"
+            if (file.exists(fpath)) { unlink(fpath) }
+            y <- matrix(rnorm(1000000), nrow=100)
+            X <- writeHDF5Array(a, fpath, name="yay", chunk_dim=c(100, 100), level=6)
         }
-        out[i] <- total
-    }
-})
 
-# (2) Semi-vectorized version of taking the rowSums.
-# Here, the operation inside the loop is vectorized.
-system.time({
-    out2 <- numeric(nrow(a))
-    for (i in seq_len(nrow(a))) {
-        out[i] <- sum(a[i,])
-    }
-})
+        N <- ifelse(mode=="hdf5", 1, 10)
+        beach.time[[mode]][it] <- timeExprs(BeachmatColSum(X), times=N)
 
-# (3) Completely vectorized.
-system.time(out3 <- rowSums(a))
+        # Block processing with varying block sizes.
+        # We use 'as.matrix' here to reflect the fact that we're using a 
+        # single C(++) implementation of an arbitrary function, and 
+        # coercing everything into a dense array to run it.
+        for (b in seq_along(block.sizes)) { 
+            block <- block.sizes[b]
+            default.time[[mode]][it,b] <- timeExprs({
+                nblocks <- ncol(X)/block
+                for (x in seq_len(nblocks)) { 
+                    base::colSums(as.matrix(X[,(x-1)*block + seq_len(block)]))
+                }
+            }, times=N)
+        }
+    }
+}
+
+overwrite <- TRUE
+for(mode in names(beach.time)) {
+    writeToFile(Type=mode, Block=NA, timings=beach.time[[mode]], file="timings_block.txt", overwrite=overwrite)
+    overwrite <- FALSE
+    for (b in seq_along(block.sizes)) {
+        writeToFile(Type=mode, Block=block.sizes[b], timings=default.time[[mode]][,b], file="timings_block.txt", overwrite=overwrite)
+    }
+}
 
