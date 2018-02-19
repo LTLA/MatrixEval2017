1b655952e77b04f47ecd996cd4282fce33dbd84c
Aaron Lun <alun@wehi.edu.au>
2018-02-19 10:30:49 +0000

    Switched to vector output for 10X figures where possible.

--- real/10X/pics/make_pics.R
+++ real/10X/pics/make_pics.R
@@ -5,7 +5,7 @@ assignments <- readRDS("../objects/cycle_output.rds")
 X <- assignments$scores$G1
 Y <- assignments$scores$G2M
 
-png("cycle.png", width=7, height=7, units="in", res=300, pointsize=12)
+pdf("cycle.pdf")
 smoothScatter(X, Y, xlab="G1 score", ylab="G2M score", cex.axis=1.2, cex.lab=1.4)
 
 segments(-1, 0.5, 0.5, 0.5, col="red", lty=2, lwd=2)
@@ -30,29 +30,46 @@ library(viridis)
 my.cols <- rev(viridis(11))
 coldex <- findInterval(nmads, 0:10/2)
 
+png("sizefacs_points.png", width=7, height=7, units="in", res=300, pointsize=12)
+par(mar=c(0,0,0,0))
 options(bitmapType="cairo")
-png("sizefacs.png", width=7, height=7, units="in", res=300, pointsize=12)
+plot(sce$total_counts/1e3, sizeFactors(sce), log="xy", col=my.cols[coldex], 
+     xlab="", ylab="", pch=16, cex=0.5, axes=FALSE)
+dev.off()
+
+pdf("sizefacs.pdf")
 plot(sce$total_counts/1e3, sizeFactors(sce), log="xy", col=my.cols[coldex], 
      xlab=expression("Library size ("*10^3*")"), ylab="Size factor", 
-     cex.axis=1.2, cex.lab=1.4, pch=16, cex=0.2)
+     cex.axis=1.2, cex.lab=1.4, pch=16, cex=0.2, type="n")
+
+limits <- par("usr")
+library(png)
+img <- readPNG("sizefacs_points.png")
+rasterImage(img, 10^limits[1], 10^limits[3], 10^limits[2], 10^limits[4])
+box()
 dev.off()
 
 ################################################################################
 # Making a plot of the top HVGs.
 
-hvg.out <- read.table("../objects/hvg_output.txt", header=TRUE)
-is.sig <- hvg.out$FDR <= 0.05
+hvg.out <- read.table("../objects/hvg_output_endog.txt", header=TRUE)
+pois.out <- read.table("../objects/hvg_output_pois.txt", header=TRUE)
+is.sig <- hvg.out$bio > 0
 
-png("hvg.png", width=7, height=7, units="in", res=300, pointsize=12)
+pdf("hvg.pdf")
 par(mar=c(5.1, 5.1, 4.1, 2.1))
 plot(hvg.out$mean, hvg.out$total, pch=16, col="grey", 
      xlab=expression("Mean log"[2]~"expression"),
      ylab=expression("Variance of log"[2]~"expression"), 
      cex.axis=1.2, cex.lab=1.4)
 points(hvg.out$mean[is.sig], hvg.out$total[is.sig], col="orange", pch=16)
+
 o <- order(hvg.out$mean)
 lines(hvg.out$mean[o], hvg.out$tech[o], col="red", lwd=2)
-legend("bottomright", sprintf("%i HVGs out of %i genes", sum(is.sig & !is.na(is.sig)), length(is.sig)), bty="n", cex=1.1)
+o <- order(pois.out$mean)
+lines(pois.out$mean[o], pois.out$tech[o], col="dodgerblue", lwd=2)
+
+legend("topright", sprintf("%i HVGs out of %i genes", sum(is.sig & !is.na(is.sig)), length(is.sig)), bty="n", cex=1.1)
 
 # Adding the top set of genes.
 chosen <- hvg.out$Symbol[1:10]
@@ -77,8 +94,7 @@ dev.off()
 pc.mat <- reducedDim(sce)
 pc.var <- attr(pc.mat, "percentVar")
 
-options(bitmapType="cairo")
-png("pca.png", width=7, height=7, units="in", res=300, pointsize=12)
+pdf("pca.pdf")
 smoothScatter(pc.mat[,1], pc.mat[,2], colramp=colorRampPalette(c("white", "black")),
      xlab=sprintf("PC1 (%.2f%%)", pc.var[1]*100),
      ylab=sprintf("PC2 (%.2f%%)", pc.var[2]*100),
