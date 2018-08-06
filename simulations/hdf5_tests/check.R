18a1adfec890ab68c855dd5a77ce353852f8f852
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-08-06 09:20:46 +0100

    Uncommented necessary lines in HDF5 timing check, updated code.

--- simulations/hdf5_tests/check.R
+++ simulations/hdf5_tests/check.R
@@ -1,11 +1,15 @@
-#ngenes <- 10000
-#ncells <- 10000    
-#dense.counts <- matrix(rnorm(ngenes*ncells), ngenes, ncells)
+# This script checks that beachmat is correctly setting the chunk cache.
+# This enables rapid consecutive access from rectangular-chunked HDF5 files.
+# The idea is to check that access is competitive with pure row/column chunks.
+
+ngenes <- 10000
+ncells <- 10000    
+dense.counts <- matrix(rnorm(ngenes*ncells), ngenes, ncells)
 
 library(HDF5Array)
-#out.bycol <- writeHDF5Array(dense.counts, "bycol.h5", name="yyy", chunk_dim=c(ngenes, 1), level=6)
-#out.byrow <- writeHDF5Array(dense.counts, "byrow.h5", name="yyy", chunk_dim=c(1, ncells), level=6)
-#out.rect <- writeHDF5Array(dense.counts, "rect.h5", name="yyy", chunk_dim=c(100, 100), level=6)
+out.bycol <- writeHDF5Array(dense.counts, "bycol.h5", name="yyy", chunkdim=c(ngenes, 1), level=6)
+out.byrow <- writeHDF5Array(dense.counts, "byrow.h5", name="yyy", chunkdim=c(1, ncells), level=6)
+out.rect <- writeHDF5Array(dense.counts, "rect.h5", name="yyy", chunkdim=c(100, 100), level=6)
 
 out.bycol <- HDF5Array("bycol.h5", "yyy", "double")
 out.byrow <- HDF5Array("byrow.h5", "yyy", "double")
